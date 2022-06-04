39a9247beb9b3d63014d1345a3f3aa63cc01acbc
Angel Cordova <itzangelbc@gmail.com>
2022-06-04 02:56:01 -0500

    proyecto app, ppt

---
+++ app01/app/src/main/java/com/acordova/app01/AngelActivity.java
@@ -0,0 +1,14 @@
+package com.acordova.app01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AngelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_angel);
+    }
+}
