9f7bfeab0b66484c45192590d0455c339242969f
Angel Cordova <itzangelbc@gmail.com>
2022-07-04 21:46:27 -0500

    proyecto final con navigation drawer

---
+++ final_project/app/src/main/java/com/upn/final_project/MainActivity.java
@@ -0,0 +1,86 @@
+package com.upn.final_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        toogle = new ActionBarDrawerToggle(this,drawer,toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toogle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.home){
+            mostrarFragmento(new HomeFragment());
+            item.setChecked(true);
+        }
+        if(item.getItemId()==R.id.tortas){
+            mostrarFragmento(new TortasFragment());
+            item.setChecked(true);
+        }
+        if(item.getItemId()==R.id.tortas_personalizadas){
+            mostrarFragmento(new TortasPersonalizadasFragment());
+            item.setChecked(true);
+        }
+        if(item.getItemId()==R.id.postres){
+            mostrarFragmento(new PostresFragment());
+            item.setChecked(true);
+        }
+        if(item.getItemId()==R.id.bebidas){
+            mostrarFragmento(new BebidasFragment());
+            item.setChecked(true);
+        }
+        if(item.getItemId()==R.id.carrito){
+            mostrarFragmento(new CarritoFragment());
+            item.setChecked(true);
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    public void mostrarFragmento(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+    }
+
+}
